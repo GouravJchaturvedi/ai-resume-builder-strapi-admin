d963caab5b968d81e0a05be0c13c83d9715afb69
Gourav Chaturvedi <chaturvedigourav47@gmail.com>
2024-10-25 15:21:17 +0530

    Initial commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,67 @@
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface ThemeColorThemeColor extends Struct.ComponentSchema {
+  collectionName: 'components_theme_color_theme_colors';
+  info: {
+    displayName: 'themeColor';
+    icon: 'connector';
+    description: '';
+  };
+  attributes: {};
+}
+
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'book';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    description: Schema.Attribute.String;
+  };
+}
+
+export interface ExperienceExperience extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'Experience';
+    icon: 'cup';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    city: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    workSummary: Schema.Attribute.String;
+  };
+}
+
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'Skills';
+    icon: 'earth';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'theme-color.theme-color': ThemeColorThemeColor;
+      'education.education': EducationEducation;
+      'experience.experience': ExperienceExperience;
+      'skills.skills': SkillsSkills;
+    }
+  }
+}
